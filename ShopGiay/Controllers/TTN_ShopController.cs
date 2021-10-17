e05237bdbb4182da068786605999cf2c75940c4a
tranthanhnha1999 <tranthanhnha12b3@gmail.com>
2021-10-17 17:13:07 +0700

    Create git Shop

---
+++ ShopGiay/Controllers/TTN_ShopController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShopGiay.Models;
+
+namespace ShopGiay.Controllers
+{
+    public class TTN_ShopController : Controller
+    {
+        public ShopBanGiayEntities1 db = new ShopBanGiayEntities1();
+        // GET: TTN_Shop
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+        public ActionResult Product()
+        {
+
+            return View();
+        }
+        public ActionResult Details(int id)
+        {
+            
+            return View();
+        }
+    }
+}
